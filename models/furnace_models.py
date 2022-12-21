b03b1b225ab846529115ad6f4f9867c847add190
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-12-21 16:03:25 +0000

    added: models

---
+++ models/furnace_models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy import ForeignKey, func
+# from sqlalchemy.dialects.postgresql import UUID
+# from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class FurnaceIO(Base):
+    __tablename__ = "furnace"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64), unique=True, nullable=False)
+    ip = Column(String(12), unique=True, nullable=False)
+    port = Column(String(6), unique=False, nullable=False)
+    is_under_surveillance = Column(Boolean)
+
+
+class FurnaceHistory(Base):
+    id = Column(Integer, primary_key=True)
+    furnace_id = Column(Integer, ForeignKey("furnace.id", ondelete="CASCADE"))
+    temperature = Column(Integer)
+    sp = Column(Integer)
+    measured_at = Column(DateTime, server_default=func.now())
