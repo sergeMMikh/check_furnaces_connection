6ecc4fd052cadc69fe5342038d46393cc85c025f
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-12-21 17:20:50 +0000

    added: session.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from classes.furnace_io import FurnaceIO
 from models.furnace_models import Base
-from models.session import engine
+from models.session import engine, Session
+from sqlalchemy.sql import select
 
 
 def main():
@@ -8,6 +9,12 @@ def main():
 
     furnace = FurnaceIO(ip='192.168.1.250',
                         port='9003')
+
+    # conn = engine.connect()
+    #
+    # s = select(furnace)
+    #
+    # print(Session.query(FurnaceIO).all())
 
     if furnace.check_ping():
         print(furnace.get_current_data())
